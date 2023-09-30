539439c185b02270e14eb62beccc2f9d1c09b09f
Michisuke-P <michisukep@gmail.com>
2023-09-30 12:12:30 +0900

    Updates build script

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ compile() {
   pushd . 
   cd $dir
   echo "Compiling $dir"
-  rm -r baykit
-  rm -f bin/bootstrap.js
-  tsc
+  npm run clean
+  npm run build
+  npm pack
   popd
 }
 
@@ -17,6 +17,7 @@ set_ver() {
   dir=$1
   pushd . 
   cd $dir
+  cp ../../LICENSE.* ../../README.* .
   echo "Set version $dir"
   sed -e "s/\"version\": .*/\"version\": \"$version\",/" \
       -e "s/\"bayserver-\(.*\)\": .*/\"bayserver-\1\": \"$version\",/" \
@@ -32,15 +33,52 @@ set_ver() {
 version=`cat VERSION`
 echo "version=" $version
 
+package_list="
+  bayserver-core
+  bayserver-docker-ajp
+  bayserver-docker-cgi
+  bayserver-docker-fcgi
+  bayserver-docker-http
+  bayserver-docker-wordpress
+  bayserver
+"
+
+pushd .
 cd packages
-for d in *; do
+for d in ${package_list}; do
   set_ver $d
 done 
-for d in *; do
-  if [ "$d" != "bayserver" ]; then
+for d in ${package_list}; do
+#  if [ "$d" != "bayserver" ]; then
     compile $d
-  fi
+#  fi
 done 
+popd
 
-exit
+echo "*********** Creating download package **********"
+
+
+target_name=BayServer_TS-${version}
+target_dir=/tmp/$target_name
+stage_log=$stage/log
+
+rm -r ${target_dir}
+mkdir -p ${target_dir}
+
+cp -r stage/bin ${target_dir}
+cp LICENSE.BAYKIT NEWS.md README.md ${target_dir}
+mkdir -p ${target_dir}/log
+
+root=`pwd`
+cd ${target_dir}
+for pkg in ${package_list}; do
+  echo "Installing package: $pkg"
+  npm install ${root}/packages/${pkg}/${pkg}-${version}.tgz
+done
+
+npx bayserver -init
+
+cd /tmp
+tar czf ${target_name}.tgz ${target_name}
+
 
